d531767dad66d718c859ccde7e40457738f30c11
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-06-27 22:31:22 +0900

    Add Vec2 methods
    Add Vec2 methods

--- Breakout/Vec2.cpp
+++ Breakout/Vec2.cpp
@@ -24,6 +24,16 @@ Vec2& Vec2::operator+=(const Vec2& rhs) {
     y += rhs.y;
     return *this;
 }
+Vec2& Vec2::operator-=(const Vec2& rhs) {
+    x -= rhs.x;
+    y -= rhs.y;
+    return *this;
+}
+Vec2& Vec2::operator-=(float rhs) {
+    x -= rhs;
+    y -= rhs;
+    return *this;
+}
 Vec2& Vec2::add(const Vec2& rhs) {
     x += rhs.x;
     y += rhs.y;
@@ -49,9 +59,20 @@ Vec2& Vec2::resize(float length) {
     scale(length);
     return *this;
 }
+
+Vec2& Vec2::decreaseUntilZero(float diff) {
+    x -= (std::signbit(x) ? -1.0f : 1.0f) * std::max(abs(x) - diff, 0.f);
+    y -= (std::signbit(y) ? -1.0f : 1.0f) * std::max(abs(y) - diff, 0.f);
+    return *this;
+}
+
 float Vec2::dist(const Vec2& v) const {
     return sqrtf(powf(x - v.x, 2) + powf(y - v.y, 2));
 }
+Vec2 Vec2::delta(const Vec2& v) const {
+    return Vec2({abs(this->x - v.x), abs(this->y - v.y)});
+}
+
 float Vec2::length() const {
     return sqrtf(powf(x, 2) + powf(y, 2));
 }
--- Breakout/Vec2.hpp
+++ Breakout/Vec2.hpp
@@ -11,12 +11,16 @@ public:
     Vec2 operator/(const Vec2& rhs) const;
     Vec2 operator/(float rhs) const;
     Vec2& operator+=(const Vec2& rhs);
+    Vec2& operator-=(const Vec2& rhs);
+    Vec2& operator-=(float rhs);
     Vec2& add(const Vec2& rhs);
     Vec2& scale(float s);
     Vec2& rotate(float degree);
     Vec2& normalize();
     Vec2& resize(float length);
+    Vec2& decreaseUntilZero(float diff);
     float dist(const Vec2& v) const;
+    Vec2 delta(const Vec2& v const;
     float length() const;
     void set(float xin, float yin);
 
